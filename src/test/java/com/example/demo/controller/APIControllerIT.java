808f4ec43b1025e5f435e9ed60db47bc847e433c
htanivar <raviregi@gmail.com>
2019-04-06 12:47:00 +0100

    Initial commit

---
+++ src/test/java/com/example/demo/controller/APIControllerIT.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
+public class APIControllerIT {
+
+    TestRestTemplate testRestTemplate = new TestRestTemplate();
+    HttpHeaders httpHeaders = new HttpHeaders();
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void sayHello() {
+
+        HttpEntity<String> entity = new HttpEntity<>(null, httpHeaders);
+
+        ResponseEntity<String> responseEntity = testRestTemplate.exchange(createURLWithPort("/hello"), HttpMethod.GET,entity,String.class);
+
+        String expected = "Hello!!!";
+
+//      Weak Testing
+        assertEquals(expected,responseEntity.getBody());
+//      Normal Testing
+        assertThat(expected,is(responseEntity.getBody()));
+
+    }
+
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
+    }
+
+
+}
