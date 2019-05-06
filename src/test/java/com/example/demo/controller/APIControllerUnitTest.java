677410146236eead35047093c8cad41801591e9b
sureshnath <sureshnath@gmail.com>
2019-05-06 23:49:50 +0100

    clean unit test on controller

--- src/test/java/com/example/demo/controller/APIControllerUnitTest.java
+++ src/test/java/com/example/demo/controller/APIControllerUnitTest.java
@@ -1,62 +1,37 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.APIService;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {APIService.class,APIController.class})
 public class APIControllerUnitTest {
 
-    MockMvc mockMvc;
-
-    @Autowired
+    @InjectMocks
     private APIController apiController;
 
-    @MockBean
+    @Mock
     private APIService apiService;
 
-    @Before
-    public void setup(){
-        mockMvc = MockMvcBuilders.standaloneSetup(apiController).build();
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Test
+    public void sayHelloToControllerShouldReturnHello(){
+        assertThat(apiController.sayHello()).isEqualTo("Hello!!!");
     }
 
     @Test
-    public void sayHello() throws Exception {
-//        when(apiController.greeting()).thenReturn("Hello Ravi");
-
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/hello")
-                .accept(MediaType.APPLICATION_JSON))
-                .andReturn();
-
-        assertThat(mvcResult.getResponse().getContentAsString(),is("Hello!!!"));
-    }
-
-    @Test
-    public void greeting() throws Exception {
-
-        when(apiService.greetingService(anyString())).thenReturn("Hello Ravi");
-
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/greetings")
-                .accept(MediaType.APPLICATION_JSON))
-                .andReturn();
-
-        assertThat(mvcResult.getResponse().getContentAsString(),is("Hello Ravi"));
-
+    public void greetingToControllerShouldReturnWhateverServiceReturns() throws Exception {
+        String expectedValue = "MockedServiceResponseValue";
+        when(apiService.greetingService(anyString())).thenReturn(expectedValue);
+        assertThat(apiController.greeting()).isEqualTo(expectedValue);
     }
 }
