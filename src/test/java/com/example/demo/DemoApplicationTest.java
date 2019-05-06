3505ddf2a1af81d11723615e43362d75f4c1d4f2
sureshnath <sureshnath@gmail.com>
2019-05-06 23:46:56 +0100

    test naming convention to end with Test rather than Tests

--- src/test/java/com/example/demo/DemoApplicationTest.java
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 @SpringBootTest
-public class DemoApplicationTests {
+public class DemoApplicationTest {
 
     @Test
     public void contextLoads() {
